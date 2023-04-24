b9c73b89113790e4ca6d725b822d831d597f88a1
godwin-gilbert <117091940+godwin-gilbert@users.noreply.github.com>
2023-04-24 21:13:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from application import app
+from application import app, db
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
